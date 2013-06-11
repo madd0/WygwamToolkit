66e0b7dc2960d127c5abb7a7fa1402f134caa246
Mauricio DIAZ ORLICH <github@madd0.com>
2013-06-11 16:56:41 +0200

    Changed SmartDispatcher interface to detect initialization

--- WygwamToolkit.WinRT/SmartDispatcher.cs
+++ WygwamToolkit.WinRT/SmartDispatcher.cs
@@ -1,19 +1,25 @@
 ﻿//-----------------------------------------------------------------------
-// <copyright file="SmartDispatcher.cs" company="">
-//     Copyright (c) mauri_000, . All rights reserved.
+// <copyright file="SmartDispatcher.cs" company="Wygwam">
+//     Copyright (c) 2013 Wygwam.
+//     Licensed under the Microsoft Public License (Ms-PL) (the "License");
+//     you may not use this file except in compliance with the License.
+//     You may obtain a copy of the License at
+//
+//         http://opensource.org/licenses/Ms-PL.html
+//
+//     Unless required by applicable law or agreed to in writing, software
+//     distributed under the License is distributed on an "AS IS" BASIS,
+//     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//     See the License for the specific language governing permissions and
+//     limitations under the License.
 // </copyright>
 //-----------------------------------------------------------------------
 namespace Wygwam.Windows
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
     using global::Windows.ApplicationModel;
     using global::Windows.UI.Core;
     using global::Windows.UI.Xaml;
-
+    using System;
 
     /// <summary>
     /// TODO: Provide summary section in the documentation header.
@@ -25,6 +31,14 @@ namespace Wygwam.Windows
         public static bool IsDesigner
         {
             get { return DesignMode.DesignModeEnabled; }
+        }
+
+        public static bool IsInitialized
+        {
+            get
+            {
+                return _dispatcher != null;
+            }
         }
 
         public static void Initialize()
@@ -41,7 +55,7 @@ namespace Wygwam.Windows
 
         public static void BeginInvoke(Action action)
         {
-                RequireInstance();
+            RequireInstance();
 
             // If the current thread is the user interface thread, skip the
             // dispatcher and directly invoke the Action.
@@ -53,13 +67,6 @@ namespace Wygwam.Windows
             {
                 _dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => action());
             }
-        }
-
-        private static bool IsInitialized()
-        {
-            RequireInstance();
-
-            return _dispatcher != null;
         }
 
         private static void RequireInstance()
